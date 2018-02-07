a68ac7347afad8f82ce13cf1032afc7725edc558
sotona <no-email>
2018-02-08 01:39:26 +0900

    + example: signals and slots and widgets

---
+++ SignalsAndSlots2/main.cpp
@@ -0,0 +1,39 @@
+#include <QApplication>
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
+#include <QDebug>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QWidget w;
+    QVBoxLayout *layoyt = new QVBoxLayout();
+
+    QPushButton *b = new QPushButton("PushMe");
+    QPushButton *b2 = new QPushButton("Show Label");
+    QLabel *l = new QLabel("I am Label");
+
+    layoyt->addWidget(b);
+    layoyt->addWidget(l);
+    layoyt->addWidget(b2);
+    w.setLayout(layoyt);
+
+
+    QObject::connect(b, &QPushButton::clicked, l, &QLabel::hide);
+    QObject::connect(b2, &QPushButton::clicked, l, &QLabel::show);
+    // Чтобы вызвать нестандартное действие QLabel нужно создать новый класс на основе QLabel
+    // в котором определить новый метод, реализующий требуемое действие.
+
+    // Можно соединсять не только методы, но и метод с любой функцией.
+    // в том числе с лямбда функцией
+    QObject::connect(b, &QPushButton::clicked, [=](){qDebug() << "label is hide";});
+
+
+    w.show();
+
+    return a.exec();
+}
