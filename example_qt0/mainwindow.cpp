e846ce20384b8603bee4b29c0ef3166b0207b881
--help <no-email>
2018-03-17 15:04:58 +0900

    + qt example 0

---
+++ example_qt0/mainwindow.cpp
@@ -0,0 +1,44 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+// метод - обработчик собития "нажатие на кнопку" pushButton_add
+//  создаётся в дизайнере форм: ПКМ на кнопке -> перейти к слоту -> cliked()
+void MainWindow::on_pushButton_add_clicked(){
+
+    //  получить содержимое однострочного поля ввода lineEdit
+    QString s = ui->lineEdit->text() + "\n";
+
+    //  получить содержимое многострочного поля ввода TextEdit
+    QString old_text = ui->textEdit->toPlainText();
+
+    // Задать содержимое многострочного поля ввода TextEdit
+    ui->textEdit->setText( old_text + s );
+}
+
+
+// обработчик собития "нажатие на кнопку" pushButton_clear
+void MainWindow::on_pushButton_clear_clicked()
+{
+    ui->textEdit->clear();
+}
+
+
+// обработчик собития "изменение состояния" флажка (чекбокса) checkBox
+// в arg1 записано новое состояние чекбокса
+void MainWindow::on_checkBox_stateChanged(int arg1)
+{
+    ui->textEdit->setReadOnly(arg1);
+}
---
+++ example_qt0/mainwindow.h
@@ -0,0 +1,53 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QMainWindow>
+
+
+// Пространство имён описывается ещё и в автоматически генерируемом
+// при компиляции файле ui_mainwindow.h.
+// Этот файл создаётся из mainwindow.ui
+namespace Ui {
+class MainWindow;
+}
+
+
+// Класс главного окна описывается программистом,
+// но наследуется от обощёного класса "QMainWindow" - пустое окно.
+// Этот класс предоставляет базовые возможности по работе с окном,
+// отображению виджетов (элементов интерфейса)
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+
+    // метод - обработчик собития "нажатие на кнопку" pushButton_add
+    //  создаётся в дизайнере форм: ПКМ на кнопке -> перейти к слоту -> cliked()
+    void on_pushButton_add_clicked();
+
+
+    // обработчик собития "нажатие на кнопку" pushButton_clear
+    void on_pushButton_clear_clicked();
+
+
+    // обработчик собития "изменение состояния" флажка (чекбокса) checkBox
+    // в arg1 записано новое состояние чекбокса
+    void on_checkBox_stateChanged(int arg1);
+
+private:
+    // поле ui содержит все элементы интрефейса пользователя
+    // расположеные в главном окне
+    // ui - экземпляр класса MainWindow из пространства имён Ui
+
+    // Этот класс MainWindow генерируется автоматически из файла формы  mainwindow.ui
+    // Файл mainwindow.ui - это XML файл описывающий главную форму,
+    // Этот фойл редактируется в Дизайнере форм,  встроеным в  Qt Creator
+
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
