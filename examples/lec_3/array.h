f7a7d47cb403868185c2d8322de052be51901d9f
VetrovSV <32772127+VetrovSV@users.noreply.github.com>
2020-09-23 13:41:14 +0900

    + examples from lec3

---
+++ examples/lec_3/array.h
@@ -0,0 +1,11 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <string>
+#include <vector>
+
+void array2file(int* a, unsigned n, std::string filename);
+
+void vector2file(std::vector<int> vec, std::string filename);
+
+#endif // ARRAY_H
