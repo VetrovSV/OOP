b1d07dca24630799fde4173b3c31f62b4b6ce206
VetrovSV <32772127+VetrovSV@users.noreply.github.com>
2021-06-16 13:38:47 +0900

    + example С#: DataGrid + BindingList

---
+++ examples/WpfApp_DataGrid/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_DataGrid
+{   
+    /// <summary>
+    /// Клиент -- предствляет одну строку таблицы
+    /// </summary>
+    class Customer
+    {
+        private String name;
+        private int id;
+
+        public Customer(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Id { get => id; set => id = value; }
+    }
+}
---
+++ examples/WpfApp_DataGrid/DataBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;            // для наблюдаемой коллекции BindingList
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_DataGrid
+{   
+    /// <summary>
+    /// Содержит данные БД, 
+    /// данные можно подключить к GridView
+    /// </summary>
+    class DataBase
+    {
+        public BindingList<Customer> data;
+
+        public DataBase()
+        {
+            data = new BindingList<Customer>();
+        }
+
+        void save()
+        {
+            // todo: сохранение данных в файл
+        }
+
+        void load()
+        {
+            // todo: загрузка данных из файла
+        }
+
+        public void add_random_data()
+        { 
+            Random rnd = new Random();
+            List<String> names = new List<string> { "Гвидо ван Россум ", "Бьёрн Страуструп", "Деннис Ритчи", "Дональд Эрвин Кнут" };
+            data.Add(new Customer( names[rnd.Next(names.Count)], rnd.Next() ));
+        }
+    }
+}
