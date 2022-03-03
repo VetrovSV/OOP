e149f7161009cd4efde2ee54cbb82cb23c3e6023
vetrovsv <vetrovsv>
2022-03-03 12:45:51 +0900

    + C# XAML exmaple

---
+++ examples/Csharp/XamlCsharpExample/Xaml_app_example_1/HelloUsername.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Имя этого пространство имён используется при подключении этого файла
+// using
+namespace XamlCsharpAppExample
+{
+
+    // Этот класс использутся в классе MainWindow
+ 
+    /// <summary>
+    /// Описывает основную логику программы: выдаёт приветствия
+    /// </summary>
+    internal class HelloUsername
+    {
+        public string greet(string username)
+        {   if ( (username == null) | (username == "") )
+                    username = "%username";
+            return "Hello, " + username + "!";
+        }
+    }
+
+    
+}
