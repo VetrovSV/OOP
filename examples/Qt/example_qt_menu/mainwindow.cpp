0f9fb432a891d2c8d9d07905965bfa3dfa29e598
VetrovSV <32772127+VetrovSV@users.noreply.github.com>
2022-12-09 22:32:59 +0900

    Update mainwindow.cpp

--- examples/Qt/example_qt_menu/mainwindow.cpp
+++ examples/Qt/example_qt_menu/mainwindow.cpp
@@ -1,4 +1,4 @@
-#include <QFileDialog>
+#include <QFileDialog>          // библиотека для создания диалоговых окон
 #include <QTextStream>
 
 #include "mainwindow.h"
@@ -17,12 +17,23 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+/// загрузка содержимого файла в многострочное поле ввода textEdit
 void MainWindow::on_actionopen_triggered(){
+    // получение пути к домашней папке пользователя
     QString home = QDir::homePath();
-
+    
+    // функция getOpenFileName создаёт диалоговое окно для открытия существующего файла
+    // Параметры: 
+    // указатель на объект родительского окна
+    // Заголовок окна
+    // Путь к паке по умолчанию
+    // Фильтр по типам файлов
+    // После закрытия окна функция вернёт абсолютное имя файла, если он был выбран
     QString fileName = QFileDialog::getOpenFileName(this,
-        "Open Image", home, "Text Files (*.txt *.scv)");
-
+        "Open Text File", home, "Text Files (*.txt)");
+    
+    // Чтение из файла в многострочное поле ввода
     QFile f(fileName);
     if ( f.open(QFile::ReadOnly) ){
         QTextStream s(&f);
@@ -32,12 +43,17 @@ void MainWindow::on_actionopen_triggered(){
 }
 
 
+/// сохранение содержимого многострочного поля ввода textEdit в файл
 void MainWindow::on_actionsave_triggered(){
+    // получение пути к домашней папке пользователя
     QString home = QDir::homePath();
-
+    
+    // функция getSaveFileName создаёт диалоговое окно для выбора имени существующего файла или ввода нового имени
+    // возвращает абсолютный путь к файлу
     QString fileName = QFileDialog::getSaveFileName(this,
         "Open Image", home);
-
+    
+    // запись в файл 
     QFile f(fileName);
     if ( f.open(QFile::WriteOnly) ){
         QTextStream s(&f);
@@ -58,6 +74,8 @@ void MainWindow::on_actionread_only_triggered()
 
 }
 
+
+/// очистка многострочного поля ввода
 void MainWindow::on_actionclear_triggered()
 {
     ui->textEdit->clear();
