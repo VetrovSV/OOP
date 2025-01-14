93f41082ec7f2d1fca1a461d4d7122250b2430a1
VetrovSV <eridani_alpha@hotmail.com>
2025-01-14 23:51:19 +0900

    upd: Java HTTP request

--- examples/java/http_requests/src/main/java/zabgu/Main.java
+++ examples/java/http_requests/src/main/java/zabgu/Main.java
@@ -3,10 +3,10 @@ package zabgu;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 import java.io.OutputStream;
@@ -15,88 +15,69 @@ import java.io.InputStream;
 public class Main {
 
     /// Пример HTTP запроса,
-    /// получает текст главной страницы сайта http://icanhazip.com/, который показывает IP адрес устройства,
-    /// с которого к нему обратились
+    /// получает текст главной страницы сайта http://icanhazip.com/.
+    /// Сайт на главной странице показывает IP адрес устройства, с которого к нему обратились.
     /// Возвращает текст ответа сервера
-    public static String simple_request() throws IOException {
-        URL url = new URL("http://icanhazip.com/");
-        // класс для отправки запросов к HTTP серверу
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        // Тип запроса - GET (см. также POST)
-        con.setRequestMethod("GET");
+    public static String simple_request() throws URISyntaxException, IOException, InterruptedException {
+        // Создаём HTTP клиент - объект, который будет выполнять веб-запросы
+        HttpClient client = HttpClient.newHttpClient();
 
-        // Запрос к серверу, получение данных:
-        // Метаинформация ответа (response)
-        System.out.println("Connection Response Message : "+con.getResponseMessage());  // Текстовый статус
-        System.out.println("Connection Response Code :    "+con.getResponseCode());     // Код. Если всё ОК, то должен быть 200
+        // Формирование запроса:
+        // Создание экземпляра HttpRequest.Builder, в конце он будет создать объекты типа HttpRequest
+        HttpRequest request = HttpRequest.newBuilder()
+                // далее идёт цепочка вызова методов:
+                .uri(new URI("http://icanhazip.com/"))      // -> Builder, в котором задан адрес
+                .GET()                                          // -> Builder, в котором задан вид запроса (GET запрос)
+                .build();                                       // -> HttpRequest
+        // Для HttpRequest нет удобного конструктора, через который можно было бы задать основные параметры запроса
+        // URI - Uniform Resource Identifier - специальный класс, который может хранить URL, адреса электронной почты, имена файлов и т.п.
 
-        // Получение тела ответа - длительная процедура. Для чтения ответа используется класс BufferedReader,
-        // который умеет читать потоковые данные
-        // InputStreamReader конвертирует полученные данные в набор символов
-        BufferedReader br = new BufferedReader(new InputStreamReader((con.getInputStream())));
+        // Отправка запроса и получение ответа
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        // HttpResponse.BodyHandlers.ofString() - создаёт специальный объект, который преобразует результат запроса в String
+        // todo: сделать обработку исключения, когда адрес недоступен или ответ не получен
 
-        // склеивание набора символов в строку
-        String output = br.lines().collect(Collectors.joining());
-        con.disconnect();
-
-        return output;
+        // Вывод статуса и тела ответа
+        System.out.println("Response Code: " + response.statusCode()); // Код ответа
+        return "Response Body: " + response.body();
     }
+    // документация по https://download.java.net/java/early_access/loom/docs/api/java.net.http/java/net/http/HttpRequest.Builder.html
+
+
+
 
 
     /// Пример HTTP запроса к серверу c LLM,
-    public static void simple_request_2() throws IOException {
+    public static void simple_request_to_llm() throws IOException, URISyntaxException, InterruptedException {
         // 1. Получение списка доступных моделей (для примера)
         // По этому запросу сервер Ollama вернёт список доступных LLM
         // см. короткую шпаргалку по Ollama: https://github.com/ivtipm/ML/blob/main/tools.md#ollama
-        URL url_llm_lst = new URL("http://localhost:11434/api/tags");
-        // класс для отправки запросов к HTTP серверу
-        HttpURLConnection con = (HttpURLConnection) url_llm_lst.openConnection();
-        // Тип запроса - GET (см. также POST)
-        con.setRequestMethod("GET");
+        HttpRequest request = HttpRequest.newBuilder().uri(new URI("http://localhost:11434/api/tags")).GET().build();
+        // Создаём HTTP клиент - объект, который будет выполнять веб-запросы
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         // Метаинформация ответа (response)
-        System.out.println("Connection Response Message : "+con.getResponseMessage());  // Текстовый статус
-        System.out.println("Connection Response Code :    "+con.getResponseCode());     // Код. Если всё ОК, то должен быть 200
-
-        // Получение тела ответа - длительная процедура. Для чтения ответа используется класс BufferedReader,
-        // который умеет читать потоковые данные
-        // InputStreamReader конвертирует полученные данные в набор символов
-        BufferedReader br = new BufferedReader(new InputStreamReader((con.getInputStream())));
-
-        // склеивание набора символов в строку
-        String output = br.lines().collect(Collectors.joining());
-//        con.disconnect();
-
-        System.out.println( "available models: " + output);
+        System.out.println("Connection Response Code : "+response.statusCode());  // Текстовый статус
+        System.out.println( "available models: " + response.body());
 
 
         //2. Отправка запроса к LLM
         String urlString = "http://localhost:11434/api/generate";                                               // Адрес для отправки запроса
-        URL url_prompt = new URL(urlString);
         String jsonPayload = "{\"model\": \"llama3.2:1b\", \"prompt\": \"Who are you?\", \"stream\": false}";   // Данные запроса
+        request = HttpRequest.newBuilder().uri(new URI(urlString)).
+                header("Content-Type", "application/json").POST(HttpRequest.BodyPublishers.ofString(jsonPayload, StandardCharsets.UTF_8))
+                .build();
+        response = client.send( request, HttpResponse.BodyHandlers.ofString() );
+        System.out.println("Response Code: " + response.statusCode());
+        System.out.println("Response: " + response.body());
+        // todo: можно использовать сторонние библиотеки для работы с ответом в JSON
 
-        // Создание и настройка нового соединения
-        con = (HttpURLConnection) url_prompt.openConnection();
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setDoOutput(true);
-        // Добавление данных в запрос
-        try (OutputStream os = con.getOutputStream()) {
-            byte[] input = jsonPayload.getBytes("utf-8");
-            os.write(input, 0, input.length); }
-
-        // Получение ответа
-        int responseCode = con.getResponseCode();
-        System.out.println("Response Code: " + responseCode);
-        System.out.println("Response: ");
-        br = new BufferedReader(new InputStreamReader((con.getInputStream())));
-        System.out.println( br.lines().collect(Collectors.joining()) );
-        con.disconnect();
     }
 
-    public static void main(String[] args) throws IOException {
-//        System.out.println( "Request result: " + simple_request() );
-        simple_request_2();
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+        System.out.println( "Request result: " + simple_request() );
+//        simple_request_to_llm();
 
     }
 }
