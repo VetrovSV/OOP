50d4126a9f539552da0ef61ff9d6c991b97c67c2
VetrovSV <eridani_alpha@hotmail.com>
2026-04-13 15:12:32 +0900

    Add Java HTTP imports and implement C++ dynamic array lab

--- plans/2026/examples/Bot/src/main/java/Zabgu/Main.java
+++ plans/2026/examples/Bot/src/main/java/Zabgu/Main.java
@@ -1,6 +1,13 @@
 package Zabgu;
 
 
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 import java.util.function.Function;
 
 public class Main {
