90077414e7139febaee199d68d380bb7f804d7f9
VetrovSV <eridani_alpha@hotmail.com>
2026-06-08 20:06:36 +0900

    upd ivtz24 examples

--- Java/2026_oop1_ivtz24/class_example/src/main/java/Library/Book.java
+++ Java/2026_oop1_ivtz24/class_example/src/main/java/Library/Book.java
@@ -1,28 +1,33 @@
-package Zabgu;
+package Library;
 
 
 // Объявление нового типа данных -- Book
+
+/// Описывает книгу
 public class Book{
     // Поля класса (переменные внутри класса)
     private int pages_count;        // количество страниц
     private String title;           // название
-    public String author;          // автор
+
+    /// автор книги
+    public String author;
     // ...
 
-    // функция инициализации -- Конструктор без параметров -- конструктор по умолчанию
+    /// функция инициализации -- Конструктор без параметров -- конструктор по умолчанию
     public Book(){
         pages_count = 0;
         title = "Без названия";
         author = "Без автора"; }
 
-    // функция инициализации -- Конструктор с параметрами
+    /// функция инициализации -- Конструктор с параметрами
     public Book( int pages_count1,  String title1, String author1){
         set_pages_count( pages_count1 );
         set_title ( title1 );
         author = author1;
     }
 
-
+    /// Задаёт количество страниц равным pages_count1, если pages_count1 >= 0
+    /// Иначе ничего не делает
     public void set_pages_count(int pages_count1 ) {
         // сеттер для поля pages_count
         if (pages_count1 >= 0) {
@@ -30,17 +35,21 @@ public class Book{
         }
     }
 
+    /// todo:
     public int get_pages_count(){
         return pages_count;}
 
+    /// todo:
     public String get_title(){
         return title;}
 
+    /// todo:
     public void set_title( String title1 ){
         if ( ! title1.equals("") ){
             title = title1;}
     }
 
+    /// Возвращает строковое представление данных (полей) класса
     public String toString(){
         String info =  String.format("%s, %s, %d стр.", title, author, pages_count );
         return info;
--- Java/2026_oop1_ivtz24/inheritance_example/src/main/java/Zabgu/Vector2D.java
+++ Java/2026_oop1_ivtz24/inheritance_example/src/main/java/Zabgu/Vector2D.java
@@ -1,4 +1,53 @@
 package Zabgu;
 
+
 public class Vector2D {
+
+    public double x;
+    public double y;
+
+    private int color;
+
+    protected String text;
+
+    public Vector2D() { x=0.0; y = 0.0; }
+
+    public Vector2D(double x1, double y1) { x=x1; y = y1; }
+
+    public double abs(){
+        return Math.sqrt( x*x + y*y);
+    }
+
+    @Override
+    public String toString(){
+        return String.Format("(%.2f, %.2f,)", x, y);
+    }
+}
+
+
+
+// Vector3D -- потомок (производный класс)
+// Vector2D -- предок (базовый класс)
+public class Vector3D extends Vector2D {
+    public double z;
+
+    public Vector3D() { 
+        // Vector2D(); конструктор по умочланию базовго класса вызывается автоматически
+        z = 0.0; }
+
+    public Vector3D(double x1, double y1, double z1) { Vector2D(x1, y1); z = z1;}
+
+    
+    // переопределение метода базового класса -- создание новой версии метода, но с другим телом
+    @Override
+    public double abs(){
+        return Math.sqrt( x*x + y*y + z*z);
+    }
+
+    @Override
+    public String toString(){
+        return String.Format("(%.2f, %.2f, %.2f)", x,y,z);
+    }
+
+
 }
