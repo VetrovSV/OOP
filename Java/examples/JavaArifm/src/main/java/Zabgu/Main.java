06f704470db024a573755497befc234aee4c2afc
VetrovSV <eridani_alpha@hotmail.com>
2026-09-14 00:42:59 +0900

    Update Main.java

--- Java/examples/JavaArifm/src/main/java/Zabgu/Main.java
+++ Java/examples/JavaArifm/src/main/java/Zabgu/Main.java
@@ -8,19 +8,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scaner = new Scanner(System.in);
-        double a = 0.0;     // угол в радианах
-        double a_deg;       // угол в градусах
-        double sina = 0.0;  // синус угла в градусах
+        int x = (int) 3.3231;    // явное приведение типа из double в int
 
-        System.out.println( "Введите угол в градусах" );
-        a_deg = scaner.nextDouble();
 
-        a = a_deg / 180.0 * Math.PI;
+        // Scanner scanner = new Scanner(System.in);
+        // double a = 0.0;     // угол в радианах
+        // double a_deg;       // угол в градусах
+        // double sina = 0.0;  // синус угла в градусах
 
-        sina = Math.sin( a );
+        // System.out.println( "Введите угол в градусах" );
+        // a_deg = scanner.nextDouble();
 
-        System.out.printf("sin(%f) = %f\n", a_deg, sina);
+        // a = a_deg / 180.0 * Math.PI;
+
+        // sina = Math.sin( a );
+
+        // System.out.printf("sin(%f) = %f\n", a_deg, sina);
 
         }
     }
