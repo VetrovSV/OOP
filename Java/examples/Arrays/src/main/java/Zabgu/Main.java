f85dc1f973720861d4b88ceb7436f2c6dbfe5444
VetrovSV <eridani_alpha@hotmail.com>
2026-09-14 14:44:52 +0900

    + java arrays example

---
+++ Java/examples/Arrays/src/main/java/Zabgu/Main.java
@@ -0,0 +1,26 @@
+package Zabgu;
+
+// Заполнить массив случайными числами
+// Найти сумму квадратов элементов массива
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Язык С:        double arr[100];
+        //Язык С:        double *arr = malloc( 100 * sizeof(double));
+        //
+
+        int N = 100;
+        // Язык Java: динамический массив
+        double[] arr = new double[N];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 10.0;
+        }
+
+        
+
+
+        }
+    }
+}
